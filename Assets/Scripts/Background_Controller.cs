fce3750ae400742ceda344ea9fcd8b5674ab96eb
MariuszAKS <mariusz-k-sikora@o2.pl>
2023-07-09 09:31:41 +0200

    Sliced and upgraded background
    It now consists of sky, clouds, background and foregrond, moving at different speeds. Next I'll add a moving sun.

--- Assets/Scripts/Background_Controller.cs
+++ Assets/Scripts/Background_Controller.cs
@@ -4,31 +4,49 @@ using UnityEngine;
 
 public class Background_Controller : MonoBehaviour
 {
+    [SerializeField] float cloudsSpeed;
     [SerializeField] float backgroundSpeed;
-    GameObject Image_1, Image_2;
-    bool image_1_onTheLeft;
+    [SerializeField] float foregroundSpeed;
+
+    GameObject Sky;
+    GameObject[] Clouds;
+    GameObject[] Background;
+    GameObject[] Foreground;
     
     void Start()
     {
-        Image_1 = transform.Find("Image_1").gameObject;
-        Image_2 = transform.Find("Image_2").gameObject;
-        image_1_onTheLeft = true;
+        Sky = transform.Find("Sky").gameObject;
+        Clouds = new GameObject[2];
+        Background = new GameObject[2];
+        Foreground = new GameObject[2];
+
+        Clouds[0] = transform.Find("Clouds_1").gameObject;
+        Clouds[1] = transform.Find("Clouds_2").gameObject;
+        Background[0] = transform.Find("Background_1").gameObject;
+        Background[1] = transform.Find("Background_2").gameObject;
+        Foreground[0] = transform.Find("Foreground_1").gameObject;
+        Foreground[1] = transform.Find("Foreground_2").gameObject;
     }
 
     void Update()
     {
-        Vector3 movement = Vector3.left * backgroundSpeed * Time.deltaTime;
-        Image_1.transform.localPosition += movement;
-        Image_2.transform.localPosition += movement;
+        MovePartOfBackground(ref Clouds, cloudsSpeed);
+        MovePartOfBackground(ref Background, backgroundSpeed);
+        MovePartOfBackground(ref Foreground, foregroundSpeed);
+    }
 
-        if (Image_1.transform.localPosition.x <= -16 || Image_2.transform.localPosition.x <= -16) {
-            if (image_1_onTheLeft) {
-                Image_1.transform.localPosition += Vector3.right * 32;
-            } else {
-                Image_2.transform.localPosition += Vector3.right * 32;
-            }
-            
-            image_1_onTheLeft = !image_1_onTheLeft;
+    void MovePartOfBackground(ref GameObject[] parts, float speed) {
+        Vector3 frameMove = Vector3.left * speed * Time.deltaTime;
+
+        parts[0].transform.localPosition += frameMove;
+        parts[1].transform.localPosition += frameMove;
+
+        if (parts[0].transform.localPosition.x <= -16) {
+            parts[0].transform.localPosition += Vector3.right * 32;
+
+            GameObject temp = parts[0];
+            parts[0] = parts[1];
+            parts[1] = temp;
         }
     }
 }
